325560d76bf1b0790111c8aee032927e20bffb07
jennypham <64534681+jennyberry@users.noreply.github.com>
2023-06-17 16:08:28 +1000

    Draft Landing page and Docs tab

--- next.config.js
+++ next.config.js
@@ -2,5 +2,12 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
+module.exports = withNextra(
+  // {
+  // i18n: {
+  //   locales: ['en', 'zh', 'de'],
+  //   defaultLocale: 'en'
+  // }
+  // }
+)
 
-module.exports = withNextra()
